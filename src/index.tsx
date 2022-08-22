7be1a4771ba310ac0cb382aa18678203b3a37c06
mirobouma <mirobouma@hotmail.com>
2022-08-22 12:52:02 +0200

    fix: remove exposed findmodule calls

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,9 @@
-import {
-  definePlugin,
-  findAllModules,
-  findModule,
-  findModuleChild,
-  ServerAPI,
-} from "decky-frontend-lib";
-
+import { definePlugin, ServerAPI } from "decky-frontend-lib";
 import { FaMusic } from "react-icons/fa";
-
 import { Title } from "./components/title";
 import { Content } from "./components/content";
 import { AppContextProvider } from "./context/context";
 import * as python from "./python";
-
-declare global {
-  interface Window {
-    findAllModules: any;
-    findModule: any;
-    findModuleChild: any;
-  }
-}
-
-window.findAllModules = findAllModules;
-window.findModule = findModule;
-window.findModuleChild = findModuleChild;
 
 export default definePlugin((serverApi: ServerAPI) => {
   python.setServer(serverApi);
