866e2b035e3a2452904121c063e98cd2f0871c85
mirobouma <mirobouma@hotmail.com>
2022-08-30 15:39:18 +0200

    feat: fetch uid for dbus session address instead of hardcoded 1000

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from getpass import getpass
 from linecache import cache
 import subprocess
 import os
@@ -18,23 +19,23 @@ class Plugin:
     cacheDir : str = ""
     symLinkPath : str = ""
 
-    def _sp_player_dbus(self, command, parameters):
+    def _get_dbus_env(self):
         env = os.environ.copy()
-        env["DBUS_SESSION_BUS_ADDRESS"] = 'unix:path=/run/user/1000/bus'
+        env["DBUS_SESSION_BUS_ADDRESS"] = f'unix:path=/run/user/{os.getuid()}/bus'
+        return env
+
+    def _sp_player_dbus(self, command, parameters):
+      
         return subprocess.Popen(f"dbus-send --print-reply --dest={self.player} {MP_PATH} {MP_MEMB_PLAYER}.{command} \
-            {parameters} > /dev/null", stdout=subprocess.PIPE, shell=True, env=env, universal_newlines=True).communicate()[0]
+            {parameters} > /dev/null", stdout=subprocess.PIPE, shell=True, env=self._get_dbus_env(), universal_newlines=True).communicate()[0]
 
     def _sp_dbus(self, command, parameters):
-        env = os.environ.copy()
-        env["DBUS_SESSION_BUS_ADDRESS"] = 'unix:path=/run/user/1000/bus'
         return subprocess.Popen(f"dbus-send --print-reply --dest={self.player} {MP_PATH} {MP_MEMB}.{command} \
-            {parameters} > /dev/null", stdout=subprocess.PIPE, shell=True, env=env, universal_newlines=True).communicate()[0]
+            {parameters} > /dev/null", stdout=subprocess.PIPE, shell=True, env=self._get_dbus_env(), universal_newlines=True).communicate()[0]
 
     def _sp_dbus_set(self, command, parameters):
-        env = os.environ.copy()
-        env["DBUS_SESSION_BUS_ADDRESS"] = 'unix:path=/run/user/1000/bus'
         return subprocess.Popen(f"dbus-send --print-reply --dest={self.player} {MP_PATH} {PROP_SET_PATH} string:\"{MP_MEMB_PLAYER}\" \
-            string:\"{command}\" {parameters} ", stdout=subprocess.PIPE, shell=True, env=env, universal_newlines=True).communicate()[0]
+            string:\"{command}\" {parameters} ", stdout=subprocess.PIPE, shell=True, env=self._get_dbus_env(), universal_newlines=True).communicate()[0]
 
     async def _sp_open(self, uri):
         return self._sp_player_dbus(self, "OpenUri", f"string:{uri}")
@@ -61,43 +62,35 @@ class Plugin:
         return self._sp_dbus_set(self, "Volume", f"variant:double:{volume}")
 
     async def sp_track_status(self):
-        env = os.environ.copy()
-        env["DBUS_SESSION_BUS_ADDRESS"] = 'unix:path=/run/user/1000/bus'
         result =  subprocess.Popen(f"dbus-send --print-reply --dest={self.player} {MP_PATH} {PROP_PATH} \
             string:\"{MP_MEMB_PLAYER}\" string:'PlaybackStatus' \
             | tail -1 \
             | cut -d \"\\\"\" -f2 | tr -d \"\n\"" \
-            ,stdout=subprocess.PIPE, shell=True, env=env, universal_newlines=True).communicate()[0]
+            ,stdout=subprocess.PIPE, shell=True, env=self._get_dbus_env(), universal_newlines=True).communicate()[0]
         return result
 
     async def sp_track_progress(self):
-        env = os.environ.copy()
-        env["DBUS_SESSION_BUS_ADDRESS"] = 'unix:path=/run/user/1000/bus'
         result = subprocess.Popen(f"dbus-send --print-reply --dest={self.player} {MP_PATH} {PROP_PATH} \
             string:\"{MP_MEMB_PLAYER}\" string:'Position' \
             | tail -1 \
             | rev | cut -d' ' -f 1 | rev | tr -d \"\n\"" \
-            , stdout=subprocess.PIPE, shell=True, env=env, universal_newlines=True).communicate()[0]
+            , stdout=subprocess.PIPE, shell=True, env=self._get_dbus_env(), universal_newlines=True).communicate()[0]
         return result
 
     async def sp_get_volume(self):
-        env = os.environ.copy()
-        env["DBUS_SESSION_BUS_ADDRESS"] = 'unix:path=/run/user/1000/bus'
         result = subprocess.Popen(f"dbus-send --print-reply --dest={self.player} {MP_PATH} {PROP_PATH} \
             string:\"{MP_MEMB_PLAYER}\" string:'Volume' \
             | tail -1 \
             | rev | cut -d' ' -f 1 | rev | tr -d \"\n\"" \
-            , stdout=subprocess.PIPE, shell=True, env=env, universal_newlines=True).communicate()[0]
+            , stdout=subprocess.PIPE, shell=True, env=self._get_dbus_env(), universal_newlines=True).communicate()[0]
         return result
 
     async def sp_can_seek(self):
-        env = os.environ.copy()
-        env["DBUS_SESSION_BUS_ADDRESS"] = 'unix:path=/run/user/1000/bus'
         result = subprocess.Popen(f"dbus-send --print-reply --dest={self.player} {MP_PATH} {PROP_PATH} \
             string:\"{MP_MEMB_PLAYER}\" string:'CanSeek' \
             | tail -1 \
             | rev | cut -d' ' -f 1 | rev | tr -d \"\n\"" \
-            , stdout=subprocess.PIPE, shell=True, env=env, universal_newlines=True).communicate()[0]
+            , stdout=subprocess.PIPE, shell=True, env=self._get_dbus_env(), universal_newlines=True).communicate()[0]
         return result
 
     async def sp_test_volume_control(self):
@@ -119,18 +112,14 @@ class Plugin:
         return "false"
 
     async def sp_identity(self, orgPath: str):
-        env = os.environ.copy()
-        env["DBUS_SESSION_BUS_ADDRESS"] = 'unix:path=/run/user/1000/bus'
         result = subprocess.Popen(f"dbus-send --print-reply --dest={orgPath} {MP_PATH} {PROP_PATH} \
             string:\"{MP_MEMB}\" string:'Identity' \
             | tail -1 \
             | rev | cut -d' ' -f 1 | rev" \
-            , stdout=subprocess.PIPE, shell=True, env=env, universal_newlines=True).communicate()[0].replace("\"", "").replace("\n", "")
+            , stdout=subprocess.PIPE, shell=True, env=self._get_dbus_env(), universal_newlines=True).communicate()[0].replace("\"", "").replace("\n", "")
         return result
 
     async def get_meta_data(self):
-        env = os.environ.copy()
-        env["DBUS_SESSION_BUS_ADDRESS"] = 'unix:path=/run/user/1000/bus'
         try:
             result = subprocess.Popen(f"dbus-send --print-reply --dest={self.player} {MP_PATH} {PROP_PATH} \
                 string:\"{MP_MEMB_PLAYER}\" string:'Metadata' \
@@ -144,7 +133,7 @@ class Plugin:
                 | sed -E 's/\"$//'                              `# ...and trailing quotes.`  \
                 | sed -E 's/\"+/|/'                             `# Regard "" as seperator.`  \
                 | sed -E 's/ +/ /g'                            `# Merge consecutive spaces.`",  
-                stdout=subprocess.PIPE, shell=True, env=env, universal_newlines=True).communicate()[0]
+                stdout=subprocess.PIPE, shell=True, env=self._get_dbus_env(), universal_newlines=True).communicate()[0]
         except:
             result = "Unavailable"
         return result
@@ -172,11 +161,8 @@ class Plugin:
         return ("https://steamloopback.host/images/deckycache_musicControl/" + baseName)
 
     async def sp_list_media_players(self):
-        env = os.environ.copy()
-        env["DBUS_SESSION_BUS_ADDRESS"] = 'unix:path=/run/user/1000/bus'
-        
         result = subprocess.Popen(f"dbus-send --print-reply --dest=org.freedesktop.DBus /org/freedesktop/DBus org.freedesktop.DBus.ListNames",
-        stdout=subprocess.PIPE, shell=True, env=env, universal_newlines=True).communicate()[0]
+        stdout=subprocess.PIPE, shell=True, env=self._get_dbus_env(), universal_newlines=True).communicate()[0]
         stripped = result.split('array [')[1].split(']')[0].replace("\n", "", 1).replace("\n", ",") \
             .replace(" ", "").replace("string", "").replace("\"", "").rstrip(',')
         services = stripped.split(',')
